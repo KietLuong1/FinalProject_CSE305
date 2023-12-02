09bc77a537aacf71d7784fe5386da91fda6f5fdb
nguyenductrong1207 <131521216+nguyenductrong1207@users.noreply.github.com>
2023-12-02 17:16:14 +0700

    baoooo

--- Final Project/Java Implementation(Netbeans)/src/gui/EmployeeLogin.java
+++ Final Project/Java Implementation(Netbeans)/src/gui/EmployeeLogin.java
@@ -314,7 +314,7 @@ public class EmployeeLogin extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
        this.hide();
-        Selector frm= new Selector();
+        FirstPage frm= new FirstPage();
         frm.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
--- Final Project/Java Implementation(Netbeans)/src/gui/ManagerRegistration.java
+++ Final Project/Java Implementation(Netbeans)/src/gui/ManagerRegistration.java
@@ -371,7 +371,7 @@ public class ManagerRegistration extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         this.hide();
-        Selector frm= new Selector();
+        FirstPage frm= new FirstPage();
         frm.setVisible(true);
     }//GEN-LAST:event_jButton2ActionPerformed
 
