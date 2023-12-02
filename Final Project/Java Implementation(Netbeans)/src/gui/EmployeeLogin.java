60637956b20ccb50d0794f0eac0a3231139ec52a
nguyenductrong1207 <131521216+nguyenductrong1207@users.noreply.github.com>
2023-12-02 17:12:05 +0700

    trong bao

--- Final Project/Java Implementation(Netbeans)/src/gui/EmployeeLogin.java
+++ Final Project/Java Implementation(Netbeans)/src/gui/EmployeeLogin.java
@@ -314,7 +314,7 @@ public class EmployeeLogin extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
        this.hide();
-        Starting frm= new Starting();
+        Selector frm= new Selector();
         frm.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
